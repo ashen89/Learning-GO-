b1af2147b1199b43cd45026e2c99d2c307c77e35
Ashen Sachintha <ashen.sachintha@xyicon.com>
2025-03-23 22:24:22 +0530

    Hello world

---
+++ Experimenting/hello-world.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var helloWorld = "Hello World"
+	helloWorld = "jjj"
+	fmt.Println(helloWorld)
+
+	fmt.Println("Hello" + "World")
+
+	fmt.Println(6 + 8)
+
+	fmt.Println(true && false)
+}
